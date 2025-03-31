fc858148cba1a7676cbd2823b22eb8481581f9cd
Sitchunyin <160621379+Sitchunyin@users.noreply.github.com>
2025-04-01 01:05:56 +0800

    Update tasks.py
    Signed-off-by: Sitchunyin <160621379+Sitchunyin@users.noreply.github.com>

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -67,6 +67,9 @@ from ultralytics.nn.modules import (
     GSConv,
     C3k2_Faster_CGLU,
     C3k2_Faster_CGLU_v2,
+    VoVGSCSP,
+    VoVGSCSPC,
+
 )
 from ultralytics.utils import DEFAULT_CFG_DICT, DEFAULT_CFG_KEYS, LOGGER, colorstr, emojis, yaml_load
 from ultralytics.utils.checks import check_requirements, check_suffix, check_yaml
